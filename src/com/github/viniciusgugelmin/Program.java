b15fc5a92dafba3eed94957fe3d1fd73affdb3ef
Vinicius <vinigugelmin@gmail.com>
2020-09-12 17:32:06 -0300

    Greetings question

--- src/com/github/viniciusgugelmin/Program.java
+++ src/com/github/viniciusgugelmin/Program.java
@@ -1,5 +1,30 @@
 package com.github.viniciusgugelmin;
 
-public class Program {
+import java.util.Scanner;
 
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		String toDoOpt = "";
+		
+		/* Greetings */
+		System.out.println("Welcome to 'Positivo restaurant'...");
+		System.out.println("Wanna take a sit? Or are you going to work today? [ANS: sit OR work]");
+		
+		do {
+			toDoOpt = in.nextLine().toLowerCase();
+			
+			if (!toDoOpt.equals("sit") && !toDoOpt.equals("work")) 
+				System.out.println("ERROR: Invalid option. [MUST BE: sit OR work]");
+			
+		} while (!toDoOpt.equals("sit") && !toDoOpt.equals("work"));
+		
+		if (toDoOpt.equals("sit")) {
+			
+		} else {
+			
+		} 
+	}
 }
